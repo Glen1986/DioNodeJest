8c1de207444fc178a43966b1c5b02f277dc21a1a
Glen112 <escobar.glen@gmail.com>
2022-07-20 07:46:51 -0300

    DioNodeJest-users-validate

--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,16 @@ const database = ['hola']
 routes.get('/users',(req, res)=>{ 
   return res.status(200).json(database)
 })
+
 routes.post('/users', (req, res) => {
   const {name} = req.body
+
+  if(name.length < 1){
+    return res.status(403).json({mensage: 'no se permite usuario vacio'})
+  }
     database.push(name)
     // console.log(database);
-  const respuesta =res.status(201).json({'mensage': `usuario ${name} creado`})
+  const respuesta =res.status(201).json({'mensage': `usuario: ${name} creado`})
   return respuesta
 })
 // GET - leer datos,

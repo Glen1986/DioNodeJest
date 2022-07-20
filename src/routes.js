784a621e72302328e2659d90f444365f1b5615ac
Glen112 <escobar.glen@gmail.com>
2022-07-20 08:38:49 -0300

    DioNodeJest-users-list

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,12 @@
 import { Router } from "express";
+import { usersController } from "./controllers/usersController.js";
 
 const routes = Router();
 
-const database = ['hola']
 
-routes.get('/users',(req, res)=>{ 
-  return res.status(200).json(database)
-})
+routes.get('/users', usersController.listarUsuario)
 
-routes.post('/users', (req, res) => {
-  const {name} = req.body
-
-  if(name.length < 1){
-    return res.status(403).json({mensage: 'no se permite usuario vacio'})
-  }
-    database.push(name)
-    // console.log(database);
-  const respuesta =res.status(201).json({'mensage': `usuario: ${name} creado`})
-  return respuesta
-})
+routes.post('/users', usersController.crearUsuario )
 // GET - leer datos,
 // POST - crear datos,
 // PUT/PATCH - editar datos,

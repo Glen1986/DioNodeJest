e406d8c74694634f61aad7193c9c21937b31bd7e
Glen112 <escobar.glen@gmail.com>
2022-07-22 14:29:43 -0300

    dio-node-jest-typescript-test

--- src/controllers/usersController.test.ts
+++ src/controllers/usersController.test.ts
@@ -1,36 +1,26 @@
+import {Request, Response} from 'express';
+import {makeMockResponse} from '../mocks/mocksResponse';
+import {UsersController} from "./usersController";
 
-import { Request } from 'express'
-import { makeMockResponse } from '../mocks/mockResponse';
-import { UsersController } from "./usersController";
-
-describe('Users Controller', ()=>{
+describe('Users Controller', () => {
   const usersController = new UsersController();
 
   const mockRequest = {} as Request
-  const mockResponse = makeMockResponse()
-  it('Deve listar os nossos usuários', ()=> {
-    usersController.listarUsuario(mockRequest, mockResponse)
+  const mockResponse = makeMockResponse() 
+  it('need to list users', () => {
+    usersController. listarUsuario(mockRequest , mockResponse)
     expect(mockResponse.state.status).toBe(200)
-    expect(mockResponse.state.json).toHaveLength(4)
+    expect(mockResponse.state.json).toHaveLength(2)
   })
+  it('need to create a new user', () => {
 
-  it('Deve criar um novo usuário', ()=> {
     mockRequest.body = {
-      name: 'Novo usuário'
+      name: 'new user'
     }
 
-    usersController.criarUsuario(mockRequest, mockResponse)
+    usersController.crearUsuario(mockRequest, mockResponse)
     expect(mockResponse.state.status).toBe(201)
-    expect(mockResponse.state.json).toMatchObject({'mensagem': `Usuário Novo usuário criado`})
+    expect(mockResponse.state.json).toMatchObject({'mensagem': `Usuário new user criado`})
   })
-
-  it('Não deve criar um usuário com o nome em branco', () => {
-    mockRequest.body = {
-      name: ''
-    }
-
-    usersController.criarUsuario(mockRequest, mockResponse)
-    expect(mockResponse.state.status).toBe(403)
-    expect(mockResponse.state.json).toMatchObject({mensagem: 'Não é possível criar usuários sem um nome'})
-  })
+  
 })
